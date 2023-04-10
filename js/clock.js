44a4de528864c431d1daa339a193d64b970a1be1
Kim So-Myoung <99127970+So-Myoung@users.noreply.github.com>
2023-04-10 21:32:06 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -4,10 +4,12 @@ function getClock(){
     var date = new Date();
     // console.log(date);
     var years = String(date.getFullYear());
+    var months = String(date.getMonth()+1);
+    var dates = String(date.getDate());
     var hours = String(date.getHours()).padStart(2, "0");
     var minutes = String(date.getMinutes()).padStart(2, "0");
     var seconds = String(date.getSeconds()).padStart(2, "0");
-    clock.innerText = `${years}년 ${hours}시 ${minutes}분 ${seconds}초 현재 환율은?`;
+    clock.innerText = `${years}-${months}-${dates} ${hours}:${minutes}:${seconds} 현재 환율은?`;
 }
 
 getClock();
